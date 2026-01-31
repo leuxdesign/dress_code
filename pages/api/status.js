68a57e8310a72d31d54186d75a37874db4b2ff3e
leuxdesign <leandrosilvactt@gmail.com>
2026-01-31 11:19:59 -0300

    ajusta api status e ambiente local

---
+++ pages/api/status.js
@@ -0,0 +1,3 @@
+export default function status(req, res) {
+  res.status(200).send("deu tudo certo com a sua requisição");
+}
---
+++ tests/unit/calculadora.test.js
@@ -0,0 +1,13 @@
+const calculadora = require("../../pages/models/calculadora.js");
+
+test("somar 2+2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  console.log(resultado);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5+100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  console.log(resultado);
+  expect(resultado).toBe(105);
+});
